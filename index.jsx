f6ffa5765cfe3e0dd384dc6c5e1f6cfda0b7ac09
finalfikh <67517895+finalfikh@users.noreply.github.com>
2023-10-24 17:27:55 +0700

    geolution landing pages

---
+++ index.jsx
@@ -0,0 +1,288 @@
+import React from "react";
+import { Button } from "./Button";
+import { Card } from "./Card";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div-2">
+        <p className="text-wrapper-2">Suply Chain Optimization to Logistic Business</p>
+        <div className="overlap-2">
+          <div className="text-wrapper-3">Nama produk</div>
+          <div className="text-wrapper-4">Why Spatial Analysis ?</div>
+          <p className="text-wrapper-5">it’s not only where, but also about why we make this decision</p>
+          <div className="hero">
+            <div className="hero-image" />
+            <div className="frame-2" />
+          </div>
+          <div className="frame-3" />
+          <Button
+            className="button-instance"
+            divClassName="design-component-instance-node"
+            size="medium"
+            variant="primary"
+          />
+        </div>
+        <p className="outcome-centered-pro">
+          Suply Chain Optimization to ensure optimal operation of the production and distribution supply chain.
+        </p>
+        <p className="text-wrapper-6">What Is Your Industry Sector ?</p>
+        <p className="outcome-centered-pro-2">
+          if you want to reduce your business loss. know 80% of data collected by businesses has a location component,
+          but only 10% is used to drive decision-making. a business model that will be built by you with excellent data
+          visualization so that you can also analyze and study competitors so that they are able to create their own
+          value for their company. With Location Intelligence, those of you who are starting a business can learn
+          customer activities and habits so as to create improvement personas for sales, marketing and customer service.
+        </p>
+        <div className="text-wrapper-7">Our Features :</div>
+        <p className="text-wrapper-8">What are They Saying :</p>
+        <div className="overlap-3">
+          <div className="text-wrapper-9">Partner :</div>
+          <div className="features-section">
+            <div className="frame-wrapper">
+              <div className="frame-4">
+                <div className="group-3">
+                  <img className="img" alt="Chating circle" src="chating-circle.png" />
+                </div>
+                <div className="text-wrapper-10">Feature</div>
+                <p className="text-wrapper-11">
+                  Tutorial meditasi dengan panduan yang mudah diikuti dimanapun kamu berada
+                </p>
+              </div>
+            </div>
+            <div className="overlap-group-2">
+              <div className="card-2">
+                <div className="frame-4">
+                  <div className="group-3">
+                    <img className="img" alt="Chating circle" src="image.png" />
+                  </div>
+                  <div className="text-wrapper-10">Feature</div>
+                  <p className="text-wrapper-11">
+                    Tutorial meditasi dengan panduan yang mudah diikuti dimanapun kamu berada
+                  </p>
+                </div>
+              </div>
+              <Card className="card-instance" hasOverlap={false} property1="group-5542" />
+            </div>
+            <div className="text-wrapper-12">Features</div>
+            <div className="business">
+              Business Intelligence and
+              <br />
+              Location Intelligence
+            </div>
+            <div className="text-wrapper-13">Thematic Maping</div>
+            <div className="text-wrapper-14">Commercial Company</div>
+            <p className="gg-kweni-no">
+              gg. Kweni no 14G, Karangwuni, Mlati, <br />
+              Sleman, Yogyakarta
+              <br />
+              55283
+            </p>
+            <div className="text-wrapper-15">Geomarketing</div>
+            <div className="text-wrapper-16">Market Research</div>
+            <div className="text-wrapper-17">Marketing and Advertsing</div>
+            <div className="overlap-4">
+              <div className="text-wrapper-18">Urban Planing</div>
+              <div className="text-wrapper-19">Logistic</div>
+            </div>
+            <div className="overlap-5">
+              <div className="text-wrapper-20">Request Demo</div>
+              <div className="text-wrapper-21">Demo</div>
+            </div>
+            <div className="overlap-6">
+              <div className="text-wrapper-22">Career’s</div>
+              <div className="text-wrapper-23">Partnership</div>
+              <div className="text-wrapper-24">News</div>
+              <div className="frame-5">
+                <div className="text-wrapper-25">About</div>
+                <div className="text-wrapper-26">Contact</div>
+              </div>
+              <div className="text-wrapper-27">Investor’s</div>
+              <div className="text-wrapper-21">Company</div>
+            </div>
+            <div className="text-wrapper-28">Agriculture</div>
+            <div className="text-wrapper-29">Statistic and sensus</div>
+            <div className="text-wrapper-30">Use Case</div>
+            <div className="card-3">
+              <div className="frame-4">
+                <div className="group-3">
+                  <div className="chating-circle-2">
+                    <div className="group-4">
+                      <div className="ellipse-8" />
+                      <div className="ellipse-9" />
+                      <div className="ellipse-10" />
+                    </div>
+                  </div>
+                </div>
+                <div className="text-wrapper-10">Feature</div>
+                <p className="text-wrapper-11">
+                  Tutorial meditasi dengan panduan yang mudah diikuti dimanapun kamu berada
+                </p>
+              </div>
+            </div>
+          </div>
+          <img className="image" alt="Image" src="image-3.png" />
+          <img className="vector" alt="Vector" src="vector.svg" />
+          <Button className="button-2" divClassName="design-component-instance-node" size="medium" variant="primary" />
+        </div>
+        <div className="overlap-7">
+          <div className="group-5">
+            <img
+              className="element-construction"
+              alt="Element construction"
+              src="3605334-construction-digger-excavation-excavator-industry-icon-1.svg"
+            />
+            <p className="outcome-centered-pro-3">Architecture, Engineering &amp; Construction Market</p>
+          </div>
+          <div className="rectangle">
+            <img
+              className="element-business"
+              alt="Element business"
+              src="1564501-business-chart-dashboard-graph-icon-1-2.png"
+            />
+          </div>
+        </div>
+        <div className="overlap-8">
+          <div className="rectangle-2">
+            <div className="outcome-centered-pro-4">Commercial Companies</div>
+            <img
+              className="element-business"
+              alt="Element business"
+              src="1564501-business-chart-dashboard-graph-icon-1-3.svg"
+            />
+          </div>
+          <img
+            className="element-office"
+            alt="Element office"
+            src="7217372-office-business-work-workplace-home-icon-1.svg"
+          />
+        </div>
+        <div className="overlap-9">
+          <div className="rectangle-3">
+            <div className="outcome-centered-pro-4">National Geospatial Agency</div>
+            <img
+              className="element-business"
+              alt="Element business"
+              src="1564501-business-chart-dashboard-graph-icon-1-2.svg"
+            />
+          </div>
+          <img className="element-satellite" alt="Element satellite" src="6622944-satellite-signal-space-icon-1.png" />
+        </div>
+        <div className="overlap-10">
+          <div className="rectangle-3">
+            <div className="outcome-centered-pro-4">Thematic Maping</div>
+            <img
+              className="element-business"
+              alt="Element business"
+              src="1564501-business-chart-dashboard-graph-icon-1.svg"
+            />
+          </div>
+          <img
+            className="element-earth-europe"
+            alt="Element earth europe"
+            src="2633179-earth-europe-globe-planet-world-icon-2.png"
+          />
+        </div>
+        <div className="overlap-11">
+          <div className="rectangle-3">
+            <div className="outcome-centered-pro-4">Professional User Domain</div>
+            <img
+              className="element-business"
+              alt="Element business"
+              src="1564501-business-chart-dashboard-graph-icon-1-5.svg"
+            />
+          </div>
+          <img
+            className="element-location"
+            alt="Element location"
+            src="2672763-location-user-app-object-essential-icon-1.png"
+          />
+        </div>
+        <div className="overlap-12">
+          <div className="rectangle-3">
+            <p className="outcome-centered-pro-4">Location Intelligence and Business Analysis</p>
+            <img
+              className="element-business"
+              alt="Element business"
+              src="1564501-business-chart-dashboard-graph-icon-1-4.svg"
+            />
+          </div>
+          <img
+            className="element-business-2"
+            alt="Element business"
+            src="1564501-business-chart-dashboard-graph-icon-1.png"
+          />
+        </div>
+        <div className="overlap-13">
+          <div className="group-6">
+            <div className="ellipse-11" />
+            <div className="ellipse-12" />
+            <div className="ellipse-13" />
+            <div className="ellipse-14" />
+            <div className="ellipse-15" />
+          </div>
+          <img className="picture" alt="Picture" src="picture1-1.png" />
+        </div>
+        <div className="navbar">
+          <div className="navbar-wrapper">
+            <div className="navbar-2">
+              <div className="home">
+                <img className="line" alt="Line" src="line-1.svg" />
+                <div className="text-wrapper-31">Home</div>
+              </div>
+              <div className="features">Industry</div>
+              <div className="features">Use Case</div>
+              <div className="text-wrapper-32">Pricing</div>
+              <div className="blog">Company</div>
+            </div>
+          </div>
+          <div className="buttons-wrapper">
+            <div className="buttons">
+              <Button className="button-3" divClassName="button-4" size="medium" text="Log in" variant="outlined" />
+              <Button className="button-5" divClassName="button-6" size="medium" text="Get Started" variant="primary" />
+            </div>
+          </div>
+        </div>
+        <img className="element-tweet" alt="Element tweet" src="5279123-tweet-twitter-twitter-logo-icon-1.svg" />
+        <img
+          className="element-github-logo"
+          alt="Element github logo"
+          src="1964417-github-logo-media-social-icon-1.svg"
+        />
+        <img
+          className="element-play-video"
+          alt="Element play video"
+          src="5279120-play-video-youtube-youtuble-logo-icon-1.svg"
+        />
+        <img className="element-linkedin" alt="Element linkedin" src="1964405-linkedin-logo-media-social-icon-1.svg" />
+        <img
+          className="element-fb-facebook"
+          alt="Element fb facebook"
+          src="5305154-fb-facebook-facebook-logo-icon-1-2.svg"
+        />
+        <img
+          className="element-fb-facebook-2"
+          alt="Element fb facebook"
+          src="5305154-fb-facebook-facebook-logo-icon-1.svg"
+        />
+        <img
+          className="element-camera"
+          alt="Element camera"
+          src="5279112-camera-instagram-social-media-instagram-logo-icon-1.svg"
+        />
+        <Button
+          className="button-7"
+          divClassName="design-component-instance-node"
+          size="medium"
+          text="Get Started"
+          variant="primary"
+        />
+        <p className="text-wrapper-33">
+          we are a spatial analysis platform that functions to map the potential of cloud-based and spatial big data
+          businesses so that business practitioners are able to make accurate business decisions.
+        </p>
+      </div>
+    </div>
+  );
+};
